a8a5faee90bd90526f7ae6a901f422a2dfda0b80
d.marra <demetrio.marra@alten.it>
2023-06-10 23:52:53 +0200

    fix max upload size (api)

--- FFMpegUI.Api/Program.cs
+++ FFMpegUI.Api/Program.cs
@@ -1,6 +1,7 @@
 using FFMpegUI.Persistence.Mapping;
 using FFMpegUI.Services;
 using FFMpegUI.Services.Middlewares;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace FFMpegUI.Api
 {
@@ -24,10 +25,28 @@ namespace FFMpegUI.Api
             builder.Services.AddScoped<IQFileServerApiService, QFileServerApiService>();
             builder.Services.AddScoped<IFFMpegConvertingService, FFMpegConversionService>();
 
+            // Configure the maximum request body size before calling UseRouting, UseEndpoints, etc.
+            // 1_000_000_000 represents the new size limit in bytes (about 1GB in this example).
+            builder.Services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = long.MaxValue;
+            });
+
+            builder.Services.Configure<IISServerOptions>(options =>
+            {
+                options.MaxRequestBodySize = null; // 1_000_000_000;
+            });
+
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.Limits.MaxRequestBodySize = null; // 1_000_000_000;
+            });
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
 
             // Set URLs
             builder.WebHost.UseUrls("http://*:80");
