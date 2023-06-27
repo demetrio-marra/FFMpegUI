1b3854decac544a8886dd6f607483ff615408613
Demetrio Marra <demetrio.marra3@gmail.com>
2023-06-27 18:01:57 +0200

    Model of progress message for processItem

---
+++ FFMpegUI/Messages/FFMpegProcessItemMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace FFMpegUI.Messages
+{
+    public class FFMpegProcessItemMessage
+    {
+        public int ProcessItemId { get; set; }
+        public long? ConvertedFileId { get; set; }
+        public string? ConvertedFileName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool? Successfull { get; set; }
+        public string? ErrorMessage { get; set; }
+        public long? ConvertedFileSize { get; set; }
+        public string? ProgressMessage { get; set; }
+    }
+}
