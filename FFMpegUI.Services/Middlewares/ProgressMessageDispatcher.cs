d8de90bd91b02ad35008ff9a28abdf69b4ce9584
Demetrio Marra <demetrio.marra3@gmail.com>
2023-06-27 20:03:47 +0200

    fixed DispatchProcessItemProgress in ProgressMessageDispatcher

--- FFMpegUI.Services/Middlewares/ProgressMessageDispatcher.cs
+++ FFMpegUI.Services/Middlewares/ProgressMessageDispatcher.cs
@@ -1,6 +1,7 @@
 ﻿using FFMpegUI.Infrastructure;
 using FFMpegUI.Messages;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace FFMpegUI.Services.Middlewares
 {
@@ -17,7 +18,7 @@ namespace FFMpegUI.Services.Middlewares
 
         async Task IProgressMessagesDispatcher.DispatchProcessItemProgress(FFMpegProcessItemMessage message)
         {
-            var progressMessageStringContent = new StringContent(JsonConvert.SerializeObject(message));
+            var progressMessageStringContent = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, mediaType: "application/json");
             await httpClient.PostAsync("ProcessItemProgress", progressMessageStringContent);
         }
     }
