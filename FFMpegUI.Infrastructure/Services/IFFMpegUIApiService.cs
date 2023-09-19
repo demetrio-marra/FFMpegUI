7348a4e6da711315e94395314195b70e48bc26b4
Demetrio Marra <demetrio.marra3@gmail.com>
2023-09-19 23:35:58 +0200

    removed unused code

--- FFMpegUI.Infrastructure/Services/IFFMpegUIApiService.cs
+++ FFMpegUI.Infrastructure/Services/IFFMpegUIApiService.cs
@@ -4,7 +4,6 @@ namespace FFMpegUI.Services.Middlewares
 {
     public interface IFFMpegUIApiService
     {
-        Task<FFMpegConvertedFileDTO> Convert(long qFileServerId, FFMpegConvertParameters parameters);
         Task ConvertEmittingMessages(int processId, long qFileServerId, int processItemId, FFMpegConvertParameters parameters);
     }
 }
--- FFMpegUI.Services/Middlewares/FFMpegApiService.cs
+++ FFMpegUI.Services/Middlewares/FFMpegApiService.cs
@@ -16,28 +16,6 @@ namespace FFMpegUI.Services.Middlewares
         }
 
 
-        async Task<FFMpegConvertedFileDTO> IFFMpegUIApiService.Convert(long qFileServerId, FFMpegConvertParameters parameters)
-        {
-            var dto = new FFMpegConvertItemDTO
-            {
-                Parameters = parameters,
-                QFileServerFileId = qFileServerId
-            };
-
-            var jsonContent = new StringContent(JsonSerializer.Serialize(dto), Encoding.UTF8, "application/json");
-
-            // Send the HTTP request
-            var response = await httpClient.PostAsync(new Uri("Conversion", UriKind.Relative), jsonContent);
-
-            response.EnsureSuccessStatusCode();
-
-            var result = await response.Content.ReadAsStringAsync();
-            var ret = JsonSerializer.Deserialize<FFMpegConvertedFileDTO>(result);
-
-            return ret;
-        }
-
-
         async Task IFFMpegUIApiService.ConvertEmittingMessages(int processId, long qFileServerId, int processItemId, FFMpegConvertParameters parameters)
         {
             var dto = new FFMpegConvertItemDTO
